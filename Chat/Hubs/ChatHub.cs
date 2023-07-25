2d5647ea494b3d6f12fc3255a167232254e0e4a1
IsaSadigovDV <isans@code.edu.az>
2023-07-25 17:07:16 +0400

    connection created

--- Chat/Hubs/ChatHub.cs
+++ Chat/Hubs/ChatHub.cs
@@ -4,5 +4,15 @@ namespace Chat.Hubs
 {
     public class ChatHub:Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Login");
+            await base.OnConnectedAsync();
+        }
     }
 }
